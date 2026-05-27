cbb0e68b670b2f065bf499e2a654f150bfbe350f
Xie Ruicheng <x_rc@qq.com>
2026-05-27 10:24:55 +0800

    BugFix: Fix expansion

--- src/_scripts/09-devshell.sh
+++ src/_scripts/09-devshell.sh
@@ -16,37 +16,37 @@ sudo rm -rf /var/lib/apt/lists/*
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" "" --unattended
 
 git clone https://github.com/zsh-users/zsh-completions \
-    "${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions"
+    "${ZSH_CUSTOM:-${ZSH:-$HOME/.oh-my-zsh}/custom}/plugins/zsh-completions"
 git clone https://github.com/zsh-users/zsh-autosuggestions \
-    "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
-    "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' ~/.zshrc
-sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions zsh-completions)/g' ~/.zshrc
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' $HOME/.zshrc
+sed -i 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions zsh-completions)/g' $HOME/.zshrc
 
 # ── vimrc ────────────────────────────────────────────────────────────────────
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
-echo 'set mouse-=a' >> ~/.vim_runtime/my_configs.vim
+git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime
+sh $HOME/.vim_runtime/install_awesome_vimrc.sh
+echo 'set mouse-=a' >> $HOME/.vim_runtime/my_configs.vim
 
 # ── misc shell config ─────────────────────────────────────────────────────────
-echo '' >> ~/.zshrc
-echo 'setopt no_nomatch # disable * match' >> ~/.zshrc
-echo '' >> ~/.zshrc
-sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' ~/.bashrc
-mkdir -p ~/.vscode-server/data/Machine
-echo 'set -g history-limit 1000000' >> ~/.tmux.conf
-echo '' >> ~/.tmux.conf
+echo '' >> $HOME/.zshrc
+echo 'setopt no_nomatch # disable * match' >> $HOME/.zshrc
+echo '' >> $HOME/.zshrc
+sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' $HOME/.bashrc
+mkdir -p $HOME/.vscode-server/data/Machine
+echo 'set -g history-limit 1000000' >> $HOME/.tmux.conf
+echo '' >> $HOME/.tmux.conf
 
 # ── ROS2 aliases ─────────────────────────────────────────────────────────────
-echo "alias load_ros=\"source /opt/ros/${ROS_DISTRO_VAL}/setup.zsh\"" >> ~/.zshrc
-echo '' >> ~/.zshrc
-echo "alias load_ros=\"source /opt/ros/${ROS_DISTRO_VAL}/setup.bash\"" >> ~/.bashrc
-echo '' >> ~/.bashrc
+echo "alias load_ros=\"source /opt/ros/${ROS_DISTRO_VAL}/setup.zsh\"" >> $HOME/.zshrc
+echo '' >> $HOME/.zshrc
+echo "alias load_ros=\"source /opt/ros/${ROS_DISTRO_VAL}/setup.bash\"" >> $HOME/.bashrc
+echo '' >> $HOME/.bashrc
 
 # ── nvm + Node.js LTS ────────────────────────────────────────────────────────
-export PROFILE=~/.zshrc
+export PROFILE=$HOME/.zshrc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
@@ -74,11 +74,11 @@ rm "nvim-linux-${NVIM_ARCH}.tar.gz"
 sudo rsync -av --ignore-existing "nvim-linux-${NVIM_ARCH}/" /usr/local
 rm -rf "nvim-linux-${NVIM_ARCH}"
 
-git clone https://github.com/NvChad/starter ~/.config/nvim --depth 1
-echo '' >> ~/.config/nvim/init.lua
-echo '-- disable mouse' >> ~/.config/nvim/init.lua
-echo 'vim.opt.mouse = ""' >> ~/.config/nvim/init.lua
-echo '' >> ~/.config/nvim/init.lua
+git clone https://github.com/NvChad/starter $HOME/.config/nvim --depth 1
+echo '' >> $HOME/.config/nvim/init.lua
+echo '-- disable mouse' >> $HOME/.config/nvim/init.lua
+echo 'vim.opt.mouse = ""' >> $HOME/.config/nvim/init.lua
+echo '' >> $HOME/.config/nvim/init.lua
 
 nvim --headless "+Lazy! sync" +qa
 nvim --headless "+Lazy load nvim-treesitter" "+TSInstallSync! lua vim vimdoc c cpp python javascript" +qa
@@ -115,13 +115,14 @@ sudo ln -sf /usr/bin/batcat /usr/local/bin/bat
 sudo rm -rf /var/lib/apt/lists/*
 
 # ── fzf ──────────────────────────────────────────────────────────────────────
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
-cat >> ~/.zshrc << 'FZF_EOF'
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+$HOME/.fzf/install --all
+cat >> $HOME/.zshrc << 'FZF_EOF'
 export FZF_CTRL_T_OPTS="
   --walker-skip .git,node_modules,target
   --preview 'bat -n --color=always {}'
   --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
 FZF_EOF
 
 # ── eza ──────────────────────────────────────────────────────────────────────
@@ -138,26 +139,26 @@ sudo mv eza /usr/local/bin/eza
 
 # ── starship + sheldon ───────────────────────────────────────────────────────
 curl -sS https://starship.rs/install.sh | POSIXLY_CORRECT=1 bash -s -- -y
-starship preset catppuccin-powerline -o ~/.config/starship.toml
+starship preset catppuccin-powerline -o $HOME/.config/starship.toml
 
 curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
-    | bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
+    | bash -s -- --repo rossmacarthur/sheldon --to $HOME/.local/bin
 
-echo y | ~/.local/bin/sheldon init --shell zsh
+echo y | $HOME/.local/bin/sheldon init --shell zsh
 
-~/.local/bin/sheldon add omz-lib \
+$HOME/.local/bin/sheldon add omz-lib \
     --github ohmyzsh/ohmyzsh \
     --dir lib \
     --use history.zsh key-bindings.zsh clipboard.zsh completion.zsh directories.zsh
-~/.local/bin/sheldon add omz-git \
+$HOME/.local/bin/sheldon add omz-git \
     --github ohmyzsh/ohmyzsh \
     --dir plugins/git \
     --use git.plugin.zsh
-~/.local/bin/sheldon add zsh-autosuggestions \
+$HOME/.local/bin/sheldon add zsh-autosuggestions \
     --github zsh-users/zsh-autosuggestions
-~/.local/bin/sheldon add zsh-completions \
+$HOME/.local/bin/sheldon add zsh-completions \
     --github zsh-users/zsh-completions
-~/.local/bin/sheldon add zsh-syntax-highlighting \
+$HOME/.local/bin/sheldon add zsh-syntax-highlighting \
     --github zsh-users/zsh-syntax-highlighting
 
 # zoxide
