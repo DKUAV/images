f9659aaf9a12e027803eaa60e2b34a6bd8820fe4
Xie Ruicheng <x_rc@qq.com>
2026-05-28 12:55:23 +0800

    Feat: Add ffmpeg

--- src/_scripts/01-system.sh
+++ src/_scripts/01-system.sh
@@ -8,15 +8,16 @@ chmod 777 /tmp
 
 apt-get update
 apt-get -y upgrade
-apt-get -y install \
-    tzdata lsb-release wget software-properties-common gnupg \
-    locales \
-    vim git curl zip unzip trash-cli parallel libssl-dev iputils-ping \
-    build-essential ninja-build gdb systemd-coredump nfs-common cmake libopencv-dev \
-    libgflags-dev libgoogle-glog-dev libgtest-dev libgmock-dev rsync git-lfs \
-    tree tmux screen cloc acl man htop landscape-common \
-    dbus-x11 fonts-wqy-zenhei fonts-noto-cjk mesa-utils libgl1-mesa-glx pulseaudio
+apt-get -y install tzdata lsb-release wget software-properties-common gnupg locales
+
 ln -snf /usr/share/zoneinfo/${TZ_VAL} /etc/localtime
 echo ${TZ_VAL} > /etc/timezone
 locale-gen en_US.UTF-8
+
+apt-get -y install vim git curl zip unzip trash-cli parallel libssl-dev iputils-ping \
+    build-essential ninja-build gdb systemd-coredump nfs-common cmake libopencv-dev \
+    libgflags-dev libgoogle-glog-dev libgtest-dev libgmock-dev rsync git-lfs \
+    tree tmux screen cloc acl man htop landscape-common ffmpeg \
+    dbus-x11 fonts-wqy-zenhei fonts-noto-cjk mesa-utils libgl1-mesa-glx pulseaudio
+
 rm -rf /var/lib/apt/lists/*
