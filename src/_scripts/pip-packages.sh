bca2ba9bb74bb18a59b0ba3c1074bcc956c523d1
Xie Ruicheng <x_rc@qq.com>
2026-06-16 20:34:05 +0800

    Feat: Add ultralytics

--- src/_scripts/pip-packages.sh
+++ src/_scripts/pip-packages.sh
@@ -11,13 +11,13 @@ pip3 install --no-cache-dir \
     pytest pytest-xdist pytest-html pytest-mock \
     pytest-randomly pytest-timeout coverage pytest-cov filelock \
     pre-commit rich BeautifulSoup4 allure-pytest \
-    plottable matplotlib openpyxl jira python-gitlab \
+    plottable matplotlib openpyxl \
     opencv_python opencv-contrib-python \
     flake8 black ruff mypy delegator.py build \
     psycopg2-binary peewee python-calamine \
     loguru fastapi aiofiles "uvicorn[standard]" python-multipart "celery[redis]" \
     "pybind11[global]" \
     scikit-image h5py onnxruntime onnx-simplifier \
-    thop lap motmetrics filterpy
+    thop lap motmetrics filterpy ultralytics
 
 curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR=/usr/local/bin INSTALLER_NO_MODIFY_PATH=1 sh
